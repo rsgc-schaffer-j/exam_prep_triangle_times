b8ef376234710bcbf2bdbc9122a77164317bf126
Russell Gordon <rgordon@rsgc.on.ca>
2017-05-08 09:15:05 -0400

    Initial version

--- ExamTemplate/main.swift
+++ ExamTemplate/main.swift
@@ -1,12 +1,74 @@
-//
-//  main.swift
-//  ExamTemplate
-//
-//  Created by Russell Gordon on 5/8/17.
-//  Copyright © 2017 Russell Gordon. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+/*
+ 
+ ORGANIZING YOUR SOLUTION
+ 
+ A good way to orgaize your code is to separate your code into the three sections - input, process, output – as much as possible.
+ 
+ The start of a solution is implemented below. Consider all the possible inputs. Can you finish the solution?
+ 
+ */
+
+/*
+ 
+ INPUT
+ 
+ Be sure that your implementation of this section discards all the possible bad inputs the user could provide.
+ 
+ Make use of your test plan and algorithm to ensure your code is complete.
+ 
+ */
+var inputToProcess : String = ""
+
+// Loop until valid input is received
+while inputToProcess == "" {
+    
+    // Show the prompt
+    print("Ask the question here? ", terminator: "")
+    
+    // Get the user's input
+    var input : String?
+    input = readLine()
+    
+    // Use optional binding to see if the string can be unwrapped (to see if it is not nil)
+    if let notNilInput = input {
+        
+        // You probably need to add additional checks to be sure the
+        // input received is valid
+        // Add checks as needed...
+        
+        // Save the input given, as we are certain it's what we are looking for now
+        inputToProcess = notNilInput
+        
+    }
+    
+}
+
+/*
+ 
+ PROCESS
+ 
+ Here is where you implement the logic that solves the problem at hand.
+ 
+ Make use of your test plan and algorithm to ensure your code is complete.
+ 
+ */
+
+// Add 'process' code below....
+print("replace with process logic")
+
+
+/*
+ 
+ OUTPUT
+ 
+ Here is where you report the results of the 'process' section above.
+ 
+ Make use of your test plan and algorithm to ensure your code is complete.
+ 
+ */
+
+// Add 'output' code below... replace what is here as needed.
+print("The input given was: \(inputToProcess)")
 
