c87d11af56a2d37afedf80b6207cc5fb4a1b211f
Russell Gordon <rgordon@rsgc.on.ca>
2017-05-08 09:09:49 -0400

    Initial Commit

---
+++ ExamTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExamTemplate
+//
+//  Created by Russell Gordon on 5/8/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
